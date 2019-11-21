03674423ccaee035501360dd04c4bd17b7b475f9
Patricio Pérez Pinto <patodeath@gmail.com>
2019-11-21 17:24:21 -0300

    Remove some stuff Change names

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserDetailRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByUsername(String username);
 
 }
--- src/main/java/com/example/demo/service/UserDetailsServiceImpl.java
+++ src/main/java/com/example/demo/service/UserDetailsServiceImpl.java
@@ -2,9 +2,8 @@ package com.example.demo.service;
 
 import com.example.demo.model.AuthUserDetail;
 import com.example.demo.model.User;
-import com.example.demo.repository.UserDetailRepository;
+import com.example.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AccountStatusUserDetailsChecker;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -16,15 +15,11 @@ import java.util.Optional;
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
-    private UserDetailRepository userDetailRepository;
-
-    @Autowired
-    private String t1;
+    private UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Optional<User> optionalUser = userDetailRepository.findByUsername(username);
-        System.out.println(t1);
+        Optional<User> optionalUser = userRepository.findByUsername(username);
         optionalUser.orElseThrow(() -> new UsernameNotFoundException("Username or password wrong"));
 
         UserDetails userDetails = new AuthUserDetail(optionalUser.get());
